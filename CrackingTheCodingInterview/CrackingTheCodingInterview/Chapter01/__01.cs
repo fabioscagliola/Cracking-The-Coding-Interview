03be54e38f67eb6982c9e994fe694e9f890ea7b3
Fabio Scagliola <scagliola@gmail.com>
2022-04-11 16:41:28 +0200

    Question 1.1 Is Unique

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter01/__01.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter01
+{
+    /// <summary>
+    /// 
+    /// ### 1.1 Is Unique 
+    /// 
+    /// Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+    /// 
+    /// *The asymptotic computational complexity of this solution is **O(n^2^)** where **n** is the length of the string* 
+    /// 
+    /// </summary>
+    class __01
+    {
+        public static bool IsUnique(string s)
+        {
+            bool result = true;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        [TestFixture]
+        class __01Test
+        {
+            [Test]
+            public void __01_IsUnique_ReturnsFalse()
+            {
+                Assert.IsFalse(__01.IsUnique("Laura"));
+            }
+
+            [Test]
+            public void __01_IsUnique_ReturnsTrue()
+            {
+                Assert.IsTrue(__01.IsUnique("Fabio"));
+            }
+
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Cracking-The-Coding-Interview
+# Cracking the Coding Interview 
+
 189 programming questions and my solutions in C# 
+
+## 1 Arrays and Strings 
+
+### 1.1 Is Unique 
+
+Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures? 
+
+*The asymptotic computational complexity of this solution is **O(n^2^)** where **n** is the length of the string* 
+
