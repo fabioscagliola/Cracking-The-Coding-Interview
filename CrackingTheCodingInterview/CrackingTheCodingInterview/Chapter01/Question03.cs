375edb015788c0ac00b15437a95c704f7da5c7c8
Fabio Scagliola <scagliola@gmail.com>
2022-04-12 11:19:56 +0200

    Question 1.3 URLify

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter01/Question03.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter01
+{
+    /// <summary>
+    /// 
+    /// ### 1.3 URLify
+    /// 
+    /// Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
+    /// 
+    /// </summary>
+    class Question03
+    {
+        static string URLify(string s, int length)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    stringBuilder.Append("%20");
+                }
+                else
+                {
+                    stringBuilder.Append(s[i]);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        static string SortString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Sort(charArray);
+            return new string(charArray);
+        }
+
+        [TestFixture]
+        class Question03Test
+        {
+            [Test]
+            public void Question03_URLify()
+            {
+                Assert.AreEqual("Mr%20John%20Smith", Question03.URLify("Mr John Smith    ", 13));
+            }
+
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 # Cracking the Coding Interview 
 
 189 programming questions and my solutions in C# 
@@ -11,4 +12,8 @@ Implement an algorithm to determine if a string has all unique characters. What 
 ### 1.2 Check Permutation 
 
 Given two strings, write a method to decide it one is a permutation of the other. 
+
+### 1.3 URLify
+
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the additional characters, and that you are given the "true" length of the string. (Note: If implementing in Java, please use a character array so that you can perform this operation in place.)
 
