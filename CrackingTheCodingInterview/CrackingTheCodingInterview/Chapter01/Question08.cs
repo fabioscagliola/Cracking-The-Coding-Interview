88f7be97a8658decc68a7c34edfe6c5e52d2b7d8
Fabio Scagliola <scagliola@gmail.com>
2022-04-15 16:02:14 +0200

    Question 1.8 Zero Matrix

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter01/Question08.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter01
+{
+    /// <summary>
+    /// 
+    /// ### 1.8 Zero Matrix 
+    /// 
+    /// Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0. 
+    /// 
+    /// </summary>
+    class Question08
+    {
+        class Zero
+        {
+            public int X { get; protected set; }
+            public int Y { get; protected set; }
+
+            public Zero(int x, int y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        static void ZeroMatrix(int[,] matrix)
+        {
+            int M = matrix.GetLength(0);
+            int N = matrix.GetLength(1);
+
+            List<Zero> zeroes = new List<Zero>();
+
+            for (int x = 0; x < M; x++)
+            {
+                for (int y = 0; y < N; y++)
+                {
+                    if (matrix[x, y] == 0)
+                    {
+                        zeroes.Add(new Zero(x, y));
+                    }
+                }
+            }
+
+            foreach (Zero zero in zeroes)
+            {
+                for (int x = 0; x < M; x++)
+                {
+                    matrix[x, zero.Y] = 0;
+                }
+
+                for (int y = 0; y < N; y++)
+                {
+                    matrix[zero.X, y] = 0;
+                }
+            }
+        }
+
+        [TestFixture]
+        class Question07Test
+        {
+            [Test]
+            public void Question07_ZeroMatrix()
+            {
+                int[,] matrix = new int[4, 5] { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9, 10 }, { 11, 12, 0, 14, 15 }, { 16, 17, 18, 19, 20 } };
+                Question08.ZeroMatrix(matrix);
+                CollectionAssert.AreEqual(new int[4, 5] { { 1, 2, 0, 4, 5 }, { 6, 7, 0, 9, 10 }, { 0, 0, 0, 0, 0 }, { 16, 17, 0, 19, 20 } }, matrix);
+            }
+
+        }
+
+    }
+}
+
--- README.md
+++ README.md
@@ -20,6 +20,10 @@ Write a method to replace all spaces in a string with '%20'. You may assume that
 
 Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words. You can ignore casing and non-letter characters. 
 
+### 1.5 One Away 
+
+There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away. 
+
 ### 1.6 String Compression 
 
 Implement a method to perform basic string compression using the counts of repeated characters. For example, the string `aabcccccaaa` would become `a2b1c5a3`. If the "compressed" string would not become smaller than the original string, your method should return the original string. 
@@ -27,4 +31,8 @@ Implement a method to perform basic string compression using the counts of repea
 ### 1.7 Rotate Matrix 
 
 Given an image represented by an N x N matrix, where each pixel in the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place? 
+
+### 1.8 Zero Matrix 
+
+Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0. 
 
