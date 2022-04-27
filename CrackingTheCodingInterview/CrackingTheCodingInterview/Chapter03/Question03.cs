3b67a51fc94a076f1415cea3f866788042b6de92
Fabio Scagliola <scagliola@gmail.com>
2022-04-27 14:22:29 +0200

    Question 3.3 Stack of Plates

--- CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter03/Question03.cs
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter03/Question03.cs
@@ -1,4 +1,6 @@
 ﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter03
 {
@@ -6,43 +8,73 @@ namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter03
     /// 
     /// ### 3.3 – Stack of Plates 
     /// 
-    /// Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure `SetofStacks` that mimics this. `SetofStacks` should be composed of several stacks and should create a new stack once the previous one exceeds capacity. `SetofStacks.push()` and ?`SetofStacks.pop()` should behave identically to a single stack (that is, `pop()` should return the same values as it would if there were just a single stack). Implement a function `popAt(int index)` which performs a pop operation on a specific sub-stack. 
+    /// Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure `SetofStacks` that mimics this. `SetofStacks` should be composed of several stacks and should create a new stack once the previous one exceeds capacity. `SetofStacks.push()` and `SetofStacks.pop()` should behave identically to a single stack (that is, `pop()` should return the same values as it would if there were just a single stack). 
     /// 
     /// </summary>
     class Question03
     {
-        static bool StackOfPlates(string s)
+        class Stacks<T>
         {
-            bool result = true;
+            protected List<Stack<T>> stacks;
 
-            for (int i = 0; i < s.Length; i++)
+            protected int height;
+
+            public Stacks(int height)
             {
-                for (int j = i + 1; j < s.Length; j++)
-                {
-                    if (s[i] == s[j])
-                    {
-                        result = false;
-                        break;
-                    }
-                }
+                stacks = new();
+
+                this.height = height;
             }
 
-            return result;
+            public int NumberOfStacks { get { return stacks.Count; } }
+
+            public void Push(T item)
+            {
+                if (stacks.Count == 0 || stacks[^1].Count == height)
+                {
+                    stacks.Add(new Stack<T>());
+                }
+
+                stacks[^1].Push(item);
+            }
+
+            public T Pop()
+            {
+                T item = stacks[^1].Pop();
+
+                if (stacks[^1].Count == 0)
+                {
+                    stacks.Remove(stacks[^1]);
+                }
+
+                return item;
+            }
+
         }
 
         [TestFixture]
         class Question03Test
         {
             [Test]
-            public void Question03_StackOfPlates_ReturnsFalse()
+            public void Question03_Stacks_PushAndPop()
             {
-                Assert.IsFalse(Question03.StackOfPlates("Laura"));
-            }
+                const int HEIGHT = 23;
+                const int PLATES = 75;
 
-            [Test]
-            public void Question03_StackOfPlates_ReturnsTrue()
-            {
-                Assert.IsTrue(Question03.StackOfPlates("Fabio"));
+                Stacks<int> stacks = new(HEIGHT);
+
+                for (int i = 0; i < PLATES; i++)
+                {
+                    stacks.Push(i);
+                    Assert.AreEqual(Math.Floor(i / HEIGHT + 1d), stacks.NumberOfStacks);
+                }
+
+                for (int i = 0; i < PLATES; i++)
+                {
+                    int test = stacks.Pop();
+                    Assert.AreEqual(PLATES - 1 - i, test);
+                    Assert.AreEqual(Math.Floor((PLATES - 1 - i) / HEIGHT + ((PLATES - 1 - i) % HEIGHT == 0 ? 0d : 1d)), stacks.NumberOfStacks);
+                }
             }
 
         }
--- README.md
+++ README.md
@@ -63,7 +63,7 @@ Write code to partition a linked list around a value x, such that all nodes less
 
 ### 3.3 – Stack of Plates 
 
-Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure `SetofStacks` that mimics this. `SetofStacks` should be composed of several stacks and should create a new stack once the previous one exceeds capacity. `SetofStacks.push()` and ?`SetofStacks.pop()` should behave identically to a single stack (that is, `pop()` should return the same values as it would if there were just a single stack). Implement a function `popAt(int index)` which performs a pop operation on a specific sub-stack. 
+Imagine a (literal) stack of plates. If the stack gets too high, it might topple. Therefore, in real life, we would likely start a new stack when the previous stack exceeds some threshold. Implement a data structure `SetofStacks` that mimics this. `SetofStacks` should be composed of several stacks and should create a new stack once the previous one exceeds capacity. `SetofStacks.push()` and `SetofStacks.pop()` should behave identically to a single stack (that is, `pop()` should return the same values as it would if there were just a single stack). 
 
 ### 3.4 – Queue via Stacks 
 
