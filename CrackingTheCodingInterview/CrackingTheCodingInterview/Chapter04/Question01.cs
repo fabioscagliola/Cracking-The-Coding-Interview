bf8f586bac21cf5867c2def8db9edf1b8d2c4935
Fabio Scagliola <scagliola@gmail.com>
2022-05-02 19:00:52 +0200

    Question 4.1 Route Between Nodes

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Chapter04/Question01.cs
@@ -0,0 +1,90 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview.Chapter04
+{
+    /// <summary>
+    /// 
+    /// ### 4.1 Route Between Nodes 
+    /// 
+    /// Given a directed graph and two nodes (S and E), design an algorithm to find out whether there is a route from S to E. 
+    /// 
+    /// </summary>
+    class Question01
+    {
+        static bool RouteBetweenNodes<T>(Graph<T> graph, Node<T> source, Node<T> target) where T : IComparable<T>
+        {
+            return graph.DepthFirstSearch(source, target) != null;
+        }
+
+        [TestFixture]
+        class Question01Test
+        {
+            [Test]
+            public void Question01_RouteBetweenNodes()
+            {
+                Graph<int> graph = new();
+
+                Node<int> n0 = new(0);
+                Node<int> n1 = new(1);
+                Node<int> n2 = new(2);
+                Node<int> n3 = new(3);
+                Node<int> n4 = new(4);
+                Node<int> n5 = new(5);
+
+                n0.NodeList.Add(n1);
+                n0.NodeList.Add(n4);
+                n0.NodeList.Add(n5);
+
+                n1.NodeList.Add(n3);
+                n1.NodeList.Add(n4);
+
+                n2.NodeList.Add(n1);
+
+                n3.NodeList.Add(n2);
+                n3.NodeList.Add(n4);
+
+                graph.NodeList.Add(n0);
+                graph.NodeList.Add(n1);
+                graph.NodeList.Add(n2);
+                graph.NodeList.Add(n3);
+                graph.NodeList.Add(n4);
+                graph.NodeList.Add(n5);
+
+                Assert.IsTrue(RouteBetweenNodes(graph, n0, n1));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n0, n2));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n0, n3));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n0, n4));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n0, n5));
+                //Assert.IsNull(graph.DepthFirstSearch(n1, n0));
+                //Assert.IsNull(graph.DepthFirstSearch(n1, n2));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n1, n3));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n1, n4));
+                //Assert.IsNull(graph.DepthFirstSearch(n1, n5));
+                //Assert.IsNull(graph.DepthFirstSearch(n2, n0));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n2, n1));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n2, n3));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n2, n4));
+                //Assert.IsNull(graph.DepthFirstSearch(n2, n5));
+                //Assert.IsNull(graph.DepthFirstSearch(n3, n0));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n3, n1));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n3, n2));
+                //Assert.IsNotNull(graph.DepthFirstSearch(n3, n4));
+                //Assert.IsNull(graph.DepthFirstSearch(n3, n5));
+                //Assert.IsNull(graph.DepthFirstSearch(n4, n0));
+                //Assert.IsNull(graph.DepthFirstSearch(n4, n1));
+                //Assert.IsNull(graph.DepthFirstSearch(n4, n2));
+                //Assert.IsNull(graph.DepthFirstSearch(n4, n3));
+                //Assert.IsNull(graph.DepthFirstSearch(n4, n5));
+                //Assert.IsNull(graph.DepthFirstSearch(n5, n0));
+                //Assert.IsNull(graph.DepthFirstSearch(n5, n1));
+                //Assert.IsNull(graph.DepthFirstSearch(n5, n2));
+                //Assert.IsNull(graph.DepthFirstSearch(n5, n3));
+                //Assert.IsNull(graph.DepthFirstSearch(n5, n4));
+            }
+
+        }
+
+    }
+}
+
---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Node.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview
+{
+    class Node<T> where T : IComparable<T>
+    {
+        public List<Node<T>> NodeList { get; set; }
+
+        public T Value { get; set; }
+
+        public bool Visited { get; set; }
+
+        public Node(T value)
+        {
+            NodeList = new();
+
+            Value = value;
+        }
+
+        public Node<T> DepthFirstSearch(Node<T> target)
+        {
+            Visited = true;
+
+            foreach (Node<T> node in NodeList)
+            {
+                if (!node.Visited && node.Value.Equals(target.Value))
+                {
+                    return node;
+                }
+
+                node.DepthFirstSearch(target);
+            }
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+
+    }
+}
+
