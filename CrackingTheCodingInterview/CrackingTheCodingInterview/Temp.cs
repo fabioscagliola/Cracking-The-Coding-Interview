1941f9ee43c4ae5b7383160762b472c16f9b3ecd
Fabio Scagliola <scagliola@gmail.com>
2022-05-20 03:45:21 -0700

    Temp class added

---
+++ CrackingTheCodingInterview/CrackingTheCodingInterview/Temp.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace com.fabioscagliola.CrackingTheCodingInterview
+{
+    class Temp
+    {
+        static bool IsThisMyName(string name)
+        {
+            return name == "Fabio";
+        }
+
+        [TestFixture]
+        class TempTest
+        {
+            [Test]
+            public void Temp_IsThisMyName_ReturnsFalse()
+            {
+                Assert.IsFalse(Temp.IsThisMyName("Laura"));
+            }
+
+            [Test]
+            public void Temp_IsThisMyName_ReturnsTrue()
+            {
+                Assert.IsTrue(Temp.IsThisMyName("Fabio"));
+            }
+
+        }
+
+    }
+}
+
+
